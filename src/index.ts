cb151569f03656adf1d691017d694e5d256802ec
Michael <guay.me@gmail.com>
2025-08-16 09:36:59 -0400

    Export symbols

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * from "./decorators.ts";
 export * from "./auth-service.ts";
 export * from "./auth-guard.ts";
 export * from "./auth-module.ts";
+export * from "./symbols.ts";
 export type { AuthModuleAsyncOptions } from "./auth-module.ts";
